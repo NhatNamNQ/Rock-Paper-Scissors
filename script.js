87adfc674088ddf2ecc1172312d5c9265260c7ff
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2024-11-28 02:29:56 +0000

    Initialize the framework for index.html and make a function named getComputerChoice

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice == 1)
+        return "rock";
+    if (computerChoice == 2)
+        return "paper";
+    return "scissors";
+}
+
+console.log(getComputerChoice())
